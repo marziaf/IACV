7e98fcda16846bf6d91e03ea2a46292b8e122155
marziaf <maarzia.f@gmail.com>
2021-10-14 21:47:38 +0200

    tSorting

--- generate_links.py
+++ generate_links.py
@@ -5,16 +5,16 @@ from os import listdir
 from os.path import isfile, join
 
 mypath = "exports"
-files = listdir(mypath)
+files = sorted(listdir(mypath))
 
 links = ""
 for f in files:
-    links += "<a href=\"{}\">{}</a> <br>".format(f, f.split(".")[0])
+    links += "<a href=\"{}\">{}</a> <br>".format(mypath + "/" + f, f.split(".")[0])
 
 with open("index_template.html") as f:
     content = f.read()
-    print(content)
     content=content.replace("LINKS_HERE", links)
+    print("Replacing index with real links")
     with open("index.html", "w") as index:
         index.write(content)
 
